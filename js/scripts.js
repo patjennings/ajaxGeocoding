d056883ef1732e4dffe79cfce2db1ca750871268
Thomas <thomas@use-design.com>
2017-07-11 22:07:57 +0200

    éléments en dur virés

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
+var CSVPath = "villes.txt";
 var p = 0; // pointer qui s'incrémente à mesure que les requêtes sont lancées — pour naviguer dans lines[]
 var timer; // interval
 var interval = 300; // intervalle avec lequel se lance la requête vers l'API geoloc
 var lines = []; // stocke les lignes du csv
 var randomizeSamePoints = "true"; // permet d'étaler les points en random sur une zone, si la ville est la même.
+var headers;
+var villePosition;
 
 $(document).ready(function(){
   loadCSV();
@@ -11,7 +14,7 @@ $(document).ready(function(){
 function loadCSV(){
   $.ajax({
       type: "GET",
-      url: "villes.txt",
+      url: CSVPath,
       dataType: "text",
       success: function(data) {processData(data);}
    });
@@ -19,10 +22,11 @@ function loadCSV(){
 
 function processData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
+    headers = allTextLines[0].split(',');
 
     for (var i=0; i<allTextLines.length; i++) {
         var data = allTextLines[i].split(',');
+
 
         if (data.length == headers.length) {
 
@@ -30,6 +34,10 @@ function processData(allText) {
             for (var j=0; j<headers.length; j++) {
                 tarr.push(/*headers[j]+":"+*/data[j]);
 
+                // On checke laquelle est la colonne City, pour établir la référence qui nous sert plus tard
+                if(headers[j] == "VILLE" || headers[j] == "ville" || headers[j] == "Ville" || headers[j] == "CITY" || headers[j] == "city" || headers[j] == "City"){
+                  villePosition = j;
+                }
             }
             lines.push(tarr);
         }
@@ -43,13 +51,12 @@ function startRequestLauncher(){
 
 function launchRequest(){
   //https://adresse.data.gouv.fr/api
-  var rqUrl = 'https://api-adresse.data.gouv.fr/search/?q='+lines[p][2];
+  var rqUrl = 'https://api-adresse.data.gouv.fr/search/?q='+lines[p][villePosition];
 
   $.ajax({
   	type: 'GET',
   	url: rqUrl,
   	jsonpCallback: 'jsonCallback',
-
   	success: function(data)
   	{
       displayData(data);
@@ -64,63 +71,49 @@ function launchRequest(){
 
 
 function displayData(data){
-  var type = lines[p][0];
-  var ecole = lines[p][1];
-  var ville = lines[p][2];
-  var niveau = lines[p][3];
-  var circo = lines[p][4];
-  var autreVille;
+  var id = p;
+  var lat, long;
 
-
-
-  var isAtPoint = checkSame(ville);
+  var isAtPoint = checkSame(lines[p][villePosition]);//
 
   if(isAtPoint == "true" && randomizeSamePoints == "true"){
     var long = randomizePoint(data.features[0].geometry.coordinates[0]);
     var lat = randomizePoint(data.features[0].geometry.coordinates[1]);
 
-    autreVille = "doublon";
   } else{
     var long = data.features[0].geometry.coordinates[0];
     var lat = data.features[0].geometry.coordinates[1];
-    autreVille = "-";
   }
 
-  var id = p;
-
-  if(p < 1){
+  if(p == 0){
     id = "ID";
     lat = "LAT";
     long = "LONG";
   }
 
+  // construction de l'output
+  var output = '<div class="result">';
+  output += '<div class="cell">'+id+',</div>';
+  for(var i = 0 ; i < headers.length ; i++){
+    output += '<div class="cell">'+lines[p][i]+',</div>';
+  }
+  output += '<div class="cell">'+lat+',</div>';
+  output += '<div class="cell">'+long+',</div>';
+  output += '</div>';
 
-  $('#jsonp-results').append('<div class="result">'+id+', '+type+', '+ecole+', '+ville+', '+niveau+', '+circo+', '+lat+', '+long+'</div>');
+  // on écrit l'output
+  $('#jsonp-results').append(output);
 
-  // var output = '<div class="result">';
-  // output += '<div class="cell">'+id+',</div>';
-  // output += '<div class="cell">'+type+',</div>';
-  // output += '<div class="cell">'+ecole+',</div>';
-  // output += '<div class="cell">'+ville+',</div>';
-  // output += '<div class="cell">'+autreVille+',</div>';
-  // output += '<div class="cell">'+niveau+',</div>';
-  // output += '<div class="cell">'+circo+',</div>';
-  // output += '<div class="cell">'+lat+',</div>';
-  // output += '<div class="cell">'+long+'</div>';
-  // output += '</div>';
-  //
-  // $('#jsonp-results').append(output);
-
+  // on incrémente le pointer
   incrementPointer();
 }
 
-
-
 function incrementPointer(){
-  if(p >= lines.length-1){
+  if(p == lines.length-1){
     clearInterval(timer);
   } else{
     p++;
+    // console.log(lines.length);
   }
 }
 
